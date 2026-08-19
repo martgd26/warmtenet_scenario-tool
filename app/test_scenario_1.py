d7b1c5ba35f576c14b37912ed9f3ce585688fb86
Mart Giesbergen <mart.giesbergen@deltares.nl>
2026-08-19 11:42:00 +0200

    Implement core Scenario 1 energy and CO2 calculations

---
+++ app/test_scenario_1.py
@@ -0,0 +1,45 @@
+import pandas as pd
+
+from calculations.scenario_1 import (calculate_hourly_heat_demand,calculate_heatpump_electricity,calculate_total_electricity_demand,calculate_household_electricity,calculate_city_electricity_demand,calculate_city_co2_emissions,)
+
+#Electriciteitsvraag
+electricity_df = pd.read_csv("data/electricity_profile.csv",sep="\t")
+electricity_profile = electricity_df["Electriciteitsprofiel from ned.nl (standaard dag profiel)"] / electricity_df["Electriciteitsprofiel from ned.nl (standaard dag profiel)"].sum()
+
+#Warmtevraag
+heat_df = pd.read_csv("data/heat_profile.csv",sep="\t")
+heat_profile = heat_df["MW"] / heat_df["MW"].sum()
+cop = 0.45*((273+50)/(50-heat_df["°C"]))
+
+#Electriciteitsvraag huishouden
+household_electricity = calculate_household_electricity(annual_electricity_demand_kwh=2500,electricity_profile=electricity_profile,)
+
+#Warmtevraag huishouden + warmtepomp elektriciteitsvraag
+hourly_heat = calculate_hourly_heat_demand(annual_heat_demand_gj=31,heat_profile=heat_profile,)
+heatpump_electricity = calculate_heatpump_electricity(hourly_heat_demand=hourly_heat,hourly_cop=cop,)
+
+#Totale elektriciteitsvraag huishouden (electriciteit + warmtepomp)
+total_electricity = calculate_total_electricity_demand(household_electricity=household_electricity,heatpump_electricity=heatpump_electricity,)
+
+#Totale elektriciteitsvraag stad (82000 huizen)
+city_electricity = calculate_city_electricity_demand(
+    total_electricity_demand=total_electricity,
+    houses=82000,
+)
+
+co2_df = pd.read_csv(
+    "data/co2_profiles.csv",
+    sep="\t"
+)
+
+co2_profile = co2_df["2025"]
+
+city_co2 = calculate_city_co2_emissions(
+    city_electricity_demand=city_electricity,
+    co2_profile=co2_profile,
+)
+
+print(
+    f"Annual city CO2 = "
+    f"{city_co2.sum()/1000:.0f} ton/year"
+)
---
+++ calculations/scenario_1.py
@@ -0,0 +1,41 @@
+import pandas as pd
+
+
+def calculate_household_electricity(
+    annual_electricity_demand_kwh: float,
+    electricity_profile: pd.Series,
+) -> pd.Series:
+    return annual_electricity_demand_kwh * electricity_profile
+
+
+def calculate_hourly_heat_demand(
+    annual_heat_demand_gj: float,
+    heat_profile: pd.Series,
+) -> pd.Series:
+    annual_heat_demand_kwh = annual_heat_demand_gj / 0.0036
+
+    return annual_heat_demand_kwh * heat_profile
+
+def calculate_heatpump_electricity(
+    hourly_heat_demand: pd.Series,
+    hourly_cop: pd.Series,
+) -> pd.Series:
+    return hourly_heat_demand / hourly_cop
+
+def calculate_total_electricity_demand(
+    household_electricity: pd.Series,
+    heatpump_electricity: pd.Series,
+) -> pd.Series:
+    return household_electricity + heatpump_electricity
+
+def calculate_city_electricity_demand(
+    total_electricity_demand: pd.Series,
+    houses: int,
+) -> pd.Series:
+    return total_electricity_demand * houses
+
+def calculate_city_co2_emissions(
+    city_electricity_demand: pd.Series,
+    co2_profile: pd.Series,
+) -> pd.Series:
+    return city_electricity_demand * co2_profile
