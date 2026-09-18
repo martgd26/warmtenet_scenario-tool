cec9a83e8bec92e6902a01fd5a15a8cd46efe70f
Mart Giesbergen <mart.giesbergen@deltares.nl>
2026-09-18 15:32:32 +0200

    Make buttons invisible

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -10,23 +10,9 @@ from calculations.scenario_3 import run_scenario_3
 from calculations.scenario_4 import run_scenario_4
 from calculations.scenario_5 import run_scenario_5
 
-#st.set_page_config(page_title="Warmtenet Tool",layout="wide",)
-st.set_page_config(
-    page_title="Warmtenet Tool",
-    layout="wide",
-    initial_sidebar_state="expanded",
-)
+st.set_page_config(page_title="Warmtenet Tool",layout="wide",)
 
-st.markdown("""
-<style>
-[data-testid="stToolbar"] {
-    display: none;
-}
-</style>
-""", unsafe_allow_html=True)
-
-st.sidebar.write("Sidebar test")
-
+st.markdown("""<style>[data-testid="stToolbar"] {display: none;}</style>""", unsafe_allow_html=True)
 
 st.title("Warmtenetten als onderdeel van het energiesysteem")
 
